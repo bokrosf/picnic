361b6b2c3999b79810cede2aa421dd1564473b52
bokrosf <bokros.feri@hotmail.com>
2025-02-02 21:45:58 +0100

    scene_navigator added.

---
+++ src/game/scene/scene_navigator.h
@@ -0,0 +1,63 @@
+#ifndef GAME_SCENE_SCENENAVIGATOR_H
+#define GAME_SCENE_SCENENAVIGATOR_H
+
+#include <concepts>
+#include <stack>
+#include <utility>
+#include <engine/scene/scene.h>
+#include <engine/scene/scene_loader.h>
+
+namespace scene_navigator
+{
+    template<typename Scene, typename... Args>
+        requires std::derived_from<Scene, scene>
+    void push(Args &&...args);
+
+    void pop();
+    void reset_top();
+
+    template<typename Scene, typename... Args>
+        requires std::derived_from<Scene, scene>
+    void reset_root(Args &&...args);
+
+    namespace detail
+    {
+        extern std::stack<int> scenes;
+    }
+}
+
+template<typename Scene, typename... Args>
+    requires std::derived_from<Scene, scene>
+void scene_navigator::push(Args &&...args)
+{
+    using namespace detail;
+
+    scene_loader::queue([args...]()
+    {
+        int scene_id = scene_loader::load<Scene>(args...);
+        scenes.push(scene_id);
+        scene_loader::activate(scene_id);
+        scene_loader::active().initialize();
+    });
+}
+
+template<typename Scene, typename... Args>
+    requires std::derived_from<Scene, scene>
+void scene_navigator::reset_root(Args &&...args)
+{
+    using namespace detail;
+
+    scene_loader::queue([args...]()
+    {
+        scene_loader::unload_all();
+
+        while (!scenes.empty())
+        {
+            scenes.pop();
+        }
+
+        push<Scene>(std::forward<Args>(args)...);
+    });
+}
+
+#endif
