10ab9b2bfa8b127600b49d66aa0d0e613c8127a6
bokrosf <bokros.feri@hotmail.com>
2025-02-06 12:21:32 +0100

    assett module interface added.

---
+++ src/engine/assett.h
@@ -0,0 +1,19 @@
+#ifndef ENGINE_ASSETT_H
+#define ENGINE_ASSETT_H
+
+#include <optional>
+#include <string>
+#include <SDL2/SDL.h>
+
+namespace assett
+{
+    using id_type = unsigned int;
+
+    void initialize();
+    void shutdown();
+    std::optional<id_type> load(const std::string &file_path);
+    void unload_all();
+    SDL_Texture *texture(id_type id);
+}
+
+#endif
