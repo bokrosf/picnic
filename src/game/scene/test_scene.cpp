520f10138418a6db7e1cf459b10d067962b798c6
bokrosf <bokros.feri@hotmail.com>
2025-02-20 15:41:03 +0100

    Basket picking is testable but bear textures used for baskets too.

--- src/game/scene/test_scene.cpp
+++ src/game/scene/test_scene.cpp
@@ -1,9 +1,14 @@
 #include <engine/assett.h>
+#include <engine/collision/box_collider.h>
+#include <engine/collision/box_collider_renderer.h>
 #include <engine/entity.h>
 #include <engine/rendering/texture_renderer.h>
 #include <game/assett.h>
+#include <game/game_coordinator.h>
+#include <game/player/basket_picker.h>
 #include <game/player/player_controller.h>
 #include <game/scene/test_scene.h>
+#include <game/tag.h>
 
 namespace
 {
@@ -29,14 +34,28 @@ namespace
 {
     void load_assetts()
     {
-        if (auto id = assett::load(game::assett::texture::bear))
+        entity &coordinator = entity::create();
+        coordinator.add_component<game_coordinator>();
+
+        auto assett_id = assett::load(game::assett::texture::bear);
+        entity &player = entity::create();
+        texture tex{.id = *assett_id, .scale = glm::vec2(5.0F)};
+        player.add_component<texture_renderer>().material(material{.texture = tex});
+        player.transform().position(0.5F * glm::vec2(1920, 1080));
+        player.add_component<player_controller>().speed = 1920;
+        player.add_component<box_collider>(glm::vec2(25, 25));
+        player.add_component<box_collider_renderer>();
+        const unsigned int all_baskets = 4;
+        player.add_component<basket_picker>(all_baskets);
+
+        for (unsigned int i = 1; i <= all_baskets; ++i)
         {
-            entity &player = entity::create();
-            texture tex{.id = *id, .scale = 5.0F * glm::vec2(1.0, 1.0)};
-            material m{.texture = tex};
-            player.add_component<texture_renderer>().material(m);
-            player.transform().position(0.5F * glm::vec2(1920, 1080));
-            player.add_component<player_controller>().speed = 1920;
+            entity &basket = entity::create();
+            basket.tag(tag::basket);
+            basket.add_component<texture_renderer>().material(material{.texture = tex});
+            basket.transform().position(0.5F * glm::vec2(1920, 1080) + glm::vec2(i * 100, 0));
+            basket.add_component<box_collider>(glm::vec2(25, 25));
+            basket.add_component<box_collider_renderer>();
         }
     }
 }
